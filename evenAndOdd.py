20d7b48d25327a7d50873d9972d212f2467e6e60
Sreekant N <03aeries.key@icloud.com>
2025-07-03 13:39:29 +0530

    Added the files using upload

---
+++ evenAndOdd.py
@@ -0,0 +1,8 @@
+# Assignment 2, Task-1: Check if a Number is even and odd
+
+num = int(input("Enter a number: "))
+if num % 2 == 0:    
+	print(f"The number entered {num} is even.")
+else:    
+	print(f"The number entered {num} is odd.")
+print("Thank you!")
